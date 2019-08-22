fc5d599cecc17e359fa03366685d85bf3c43bb8c
SharifAbdulCoder <abdugofir@gmail.com>
2019-08-21 20:55:19 -0500

    docker install

--- jenkins/kubernetes.groovy
+++ jenkins/kubernetes.groovy
@@ -38,8 +38,27 @@ node('master') {
                 // if terraform does not installed in system stage will install the terraform
                  stage('Installing Docker') {
                    sh """
-                   sudo yum install docker -y
-                   systemctl start docker
+                   sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+                  sudo yum install -y yum-utils \
+                  device-mapper-persistent-data \
+                  lvm2
+
+                  sudo yum-config-manager \
+                  --add-repo \
+                  https://download.docker.com/linux/centos/docker-ce.repo
+
+                  sudo yum-config-manager --enable docker-ce-nightly
+                  sudo yum-config-manager --enable docker-ce-test
+                  sudo yum-config-manager --disable docker-ce-nightly
+                  sudo yum install docker-ce docker-ce-cli containerd.io
                    """
                  }
               }
